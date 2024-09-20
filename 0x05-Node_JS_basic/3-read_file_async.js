1816d04e75cdb4ad01210c64bc467288c06027d0
karxg <karim4420@aol.com>
2024-09-20 14:14:46 +0300

    proj

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,39 +1,36 @@
 const fs = require('fs');
 
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} path - The path to the CSV data file.
+ * @returns {Promise<void>}
+ */
 
-const countStudents = (dataPath) => new Promise((resolve, reject) => {
-  fs.readFile(dataPath, 'utf-8', (err, data) => {
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       reject(new Error('Cannot load the database'));
     }
     if (data) {
-      const fileLines = data
-        .toString('utf-8')
-        .trim()
-        .split('\n');
-      const studentGroups = {};
-      const dbFieldNames = fileLines[0].split(',');
-      const studentPropNames = dbFieldNames
-        .slice(0, dbFieldNames.length - 1);
+      const fileLines = data.toString('utf-8').trim().split('\n');
+      const stuObj = {};
+      const fieldNames = fileLines[0].split(',');
+      const stuPropNames = fieldNames.slice(0, fieldNames.length - 1);
 
       for (const line of fileLines.slice(1)) {
-        const studentRecord = line.split(',');
-        const studentPropValues = studentRecord
-          .slice(0, studentRecord.length - 1);
-        const field = studentRecord[studentRecord.length - 1];
-        if (!Object.keys(studentGroups).includes(field)) {
-          studentGroups[field] = [];
+        const studentRow = line.split(',');
+        const studentPropValues = studentRow.slice(0, studentRow.length - 1);
+        const field = studentRow[studentRow.length - 1];
+        if (!Object.keys(stuObj).includes(field)) {
+          stuObj[field] = [];
         }
-        const studentEntries = studentPropNames
-          .map((propName, idx) => [propName, studentPropValues[idx]]);
-        studentGroups[field].push(Object.fromEntries(studentEntries));
+        const stuEntries = stuPropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
+        stuObj[field].push(Object.fromEntries(stuEntries));
       }
 
-      const totalStudents = Object
-        .values(studentGroups)
-        .reduce((pre, cur) => (pre || []).length + cur.length);
-      console.log(`Number of students: ${totalStudents}`);
-      for (const [field, group] of Object.entries(studentGroups)) {
+      const total = Object.values(stuObj).reduce((pre, cur) => (pre || []).length + cur.length);
+      console.log(`Number of students: ${total}`);
+      for (const [field, group] of Object.entries(stuObj)) {
         const studentNames = group.map((student) => student.firstname).join(', ');
         console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
       }
--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,14 +4,12 @@ const PORT = 1245;
 const HOST = 'localhost';
 const app = http.createServer();
 
-
-app.on('request', (_, res) => {
+app.on('request', (_, response) => {
   const responseText = 'Hello Holberton School!';
-
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', responseText.length);
-  res.statusCode = 200;
-  res.write(Buffer.from(responseText));
+  response.setHeader('Content-Type', 'text/plain');
+  response.setHeader('Content-Length', responseText.length);
+  response.statusCode = 200;
+  response.write(Buffer.from(responseText));
 });
 
 app.listen(PORT, HOST, () => {
